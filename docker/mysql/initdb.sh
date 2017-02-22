aeda50d077ef9c228632bc4dfcc9c3df2cc4daa1
Luis Miguel Mejía Suárez <luis.miguel.mejia@hotmail.com>
2017-02-22 12:30:01 -0500

    Giving access to the database from any host

--- docker/mysql/initdb.sh
+++ docker/mysql/initdb.sh
@@ -14,8 +14,7 @@
 # limitations under the License.
 
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE bookstore;"
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON bookstore.* TO 'bookstore'@'localhost' IDENTIFIED BY 'bookstore';"
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON bookstore.* TO 'bookstore'@'$APACHE_HOSTS' IDENTIFIED BY 'bookstore';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON bookstore.* TO 'bookstore'@'%' IDENTIFIED BY 'bookstore';"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
 mysql -u bookstore -pbookstore bookstore < /home/bookstore/bookstore.sql
 mysql -u bookstore -pbookstore bookstore < /home/bookstore/data.sql
